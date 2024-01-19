ca15007721056731252b48f1c6ad41c8b04b55a9
Brandon Pinto <brandon.a.pinto@protonmail.com>
2024-01-19 10:34:05 -0300

    feat: add camera and player

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,37 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
 
 fn main() {
-    App::new().add_systems(Update, hello_world).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (spawn_camera, spawn_player).chain())
+        .run();
 }
 
-fn hello_world() {
-    println!("Hello World!");
+#[derive(Component)]
+pub struct Player {}
+
+pub fn spawn_player(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 10.0, 0.0),
+            texture: asset_server.load("sprites/playerShip1_blue.png"),
+            ..default()
+        },
+        Player {},
+    ));
+}
+
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..default()
+    });
 }
